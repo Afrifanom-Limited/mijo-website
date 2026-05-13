0885656ca1ca14b02eb2db07cf37b3a984587ae7
oscarlyn <oscarlyn.ghartey@afrifanom.com>
2026-05-13 09:56:13 +0000

    took out popup

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
 
   return (
     <div className="relative">
-      <PromoPopup />
+      {/* <PromoPopup /> */}
       {showBanner && (
         <ComingSoonBanner onDismiss={() => setShowBanner(false)} />
       )}
