d6c3c3112e4b13d268dbe95e8df0cebab1ccbe44
oscarlyn <oscarlyn.ghartey@afrifanom.com>
2026-07-06 13:32:41 +0000

    logo change

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,7 +1,20 @@
 import logo from "../../assets/logo.png";
 
-const Logo = ({ width = 40 }: { width?: number }) => {
-  return <img src={logo} width={width} />;
+const Logo = ({
+  width = 40,
+  onClick,
+}: {
+  width?: number;
+  onClick?: () => void;
+}) => {
+  return (
+    <img
+      src={logo}
+      width={width}
+      onClick={onClick}
+      className={onClick ? "cursor-pointer" : ""}
+    />
+  );
 };
 
 export default Logo;
